9d6cf7507e47449a7ae486aefe47707ec7310aca
Christian-Active <chrisactive06@gmail.com>
2024-03-07 14:56:06 +0100

    Basic c++ calculator

---
+++ calculator.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int calculateNum(double num1, char op, double num2) {
+
+    double result;
+
+    if (op == '+') {
+        result = num1 + num2;
+    }
+    else if (op == '-') {
+        result = num1 - num2;
+    }
+    else if (op == '/') {
+        result = num1 / num2;
+    }
+    else if (op == '*') {
+        result = num1 * num2;
+    }
+    else if (op == '^') {
+        result = pow(num1, num2);
+    }
+    else {
+        cout << "Invalid operator used: Please provide a valid operator";
+    }
+    
+    cout << "The answer is " << result;
+    return 0;
+}
+
+int main () {
+    double num1;
+    double num2;
+    char op;
+
+    cin >> num1;
+    cin >> op;
+    cin >> num2;
+
+    calculateNum(num1, op, num2);
+    return 0;
+}
